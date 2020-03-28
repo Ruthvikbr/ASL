5700f2fd98a72cd9ad9984c3d99e6c2f4eafccf8
Ruthvikbr <ruthvikbr24@gmail.com>
2020-03-29 04:57:17 +0530

    implement frame processing and firebase image labelling

--- app/src/main/java/com/signlaguage/asl/MainActivity.java
+++ app/src/main/java/com/signlaguage/asl/MainActivity.java
@@ -3,17 +3,40 @@ package com.signlaguage.asl;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.common.FirebaseMLException;
+import com.google.firebase.ml.custom.FirebaseCustomLocalModel;
+import com.google.firebase.ml.custom.FirebaseModelDataType;
+import com.google.firebase.ml.custom.FirebaseModelInputOutputOptions;
+import com.google.firebase.ml.custom.FirebaseModelInputs;
+import com.google.firebase.ml.custom.FirebaseModelInterpreter;
+import com.google.firebase.ml.custom.FirebaseModelInterpreterOptions;
+import com.google.firebase.ml.custom.FirebaseModelOutputs;
 import com.otaliastudios.cameraview.CameraView;
 import com.otaliastudios.cameraview.frame.Frame;
 import com.otaliastudios.cameraview.frame.FrameProcessor;
 import com.otaliastudios.cameraview.size.Size;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+
 public class MainActivity extends AppCompatActivity {
     CameraView camera;
+    FirebaseModelInterpreter interpreter;
+    FirebaseModelInputOutputOptions inputOutputOptions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +44,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         camera = findViewById(R.id.camera);
         camera.setLifecycleOwner(this);
+        Button start = findViewById(R.id.startCamera);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startCamera();
+            }
+        });
+        Button stop = findViewById(R.id.stopCamera);
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopCamera();
+            }
+        });
+    }
+    private void startCamera(){
         camera.addFrameProcessor(new FrameProcessor() {
             @Override
             public void process(@NonNull Frame frame) {
@@ -31,14 +70,23 @@ public class MainActivity extends AppCompatActivity {
                 int viewRotation = frame.getRotationToView();
                 if (frame.getDataClass() == byte[].class) {
                     byte[] data = frame.getData();
-                    Log.v("FrameDetails"," "+format);
+                    Bitmap bmp = BitmapFactory.decodeByteArray(data, 0, data.length);
+                    loadModel(bmp);
                 } else if (frame.getDataClass() == Image.class) {
                     Image data = frame.getData();
                     // Process android.media.Image...
                 }
             }
         });
+    }
 
+    private void stopCamera(){
+        camera.removeFrameProcessor(new FrameProcessor() {
+            @Override
+            public void process(@NonNull Frame frame) {
+
+            }
+        });
     }
 
     @Override
@@ -59,4 +107,82 @@ public class MainActivity extends AppCompatActivity {
         camera.destroy();
     }
 
+    private void loadModel(Bitmap bitmap) {
+        FirebaseCustomLocalModel localModel = new FirebaseCustomLocalModel.Builder()
+                .setAssetFilePath("model2.tflite")
+                .build();
+
+
+        try {
+            FirebaseModelInterpreterOptions options =
+                    new FirebaseModelInterpreterOptions.Builder(localModel).build();
+            interpreter = FirebaseModelInterpreter.getInstance(options);
+        } catch (FirebaseMLException e) {
+            Log.v("Firebase Exception", "" + e.getMessage());
+        }
+        try {
+            inputOutputOptions =
+                    new FirebaseModelInputOutputOptions.Builder()
+                            .setInputFormat(0, FirebaseModelDataType.FLOAT32, new int[]{1, 200, 200, 3})
+                            .setOutputFormat(0, FirebaseModelDataType.FLOAT32, new int[]{1, 29})
+                            .build();
+        } catch (FirebaseMLException e) {
+            e.printStackTrace();
+        }
+        int batchNum = 0;
+        float[][][][] input = new float[1][200][200][3];
+        for (int x = 0; x < 200; x++) {
+            for (int y = 0; y < 200; y++) {
+                int pixel = bitmap.getPixel(x, y);
+
+                input[batchNum][x][y][0] = (Color.red(pixel) - 127) / 128.0f;
+                input[batchNum][x][y][1] = (Color.green(pixel) - 127) / 128.0f;
+                input[batchNum][x][y][2] = (Color.blue(pixel) - 127) / 128.0f;
+            }
+        }
+        inputImage(input);
+
+    }
+
+    private void inputImage(float[][][][] input) {
+        FirebaseModelInputs inputs = null;
+        try {
+            inputs = new FirebaseModelInputs.Builder()
+                    .add(input)  // add() as many input arrays as your model requires
+                    .build();
+            interpreter.run(inputs, inputOutputOptions)
+                    .addOnSuccessListener(
+                            new OnSuccessListener<FirebaseModelOutputs>() {
+                                @Override
+                                public void onSuccess(FirebaseModelOutputs result) {
+                                    float[][] output = result.getOutput(0);
+                                    float[] probabilities = output[0];
+                                    Log.v("outputs"," "+ Arrays.toString(output[0]));
+                                    try{
+                                        BufferedReader reader = new BufferedReader(
+                                                new InputStreamReader(getAssets().open("labels.txt")));
+                                        for (float probability : probabilities) {
+                                            String label = reader.readLine();
+
+                                            Log.i("MLKit", String.format("%s: %1.4f", label, probability));
+                                        }
+                                    }catch(IOException e){
+                                        e.printStackTrace();
+                                    }
+                                }
+                            })
+                    .addOnFailureListener(
+                            new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    // Task failed with an exception
+                                    // ...
+                                    Log.v("Result", "" + e.getMessage());
+                                }
+                            });
+        } catch (FirebaseMLException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
